5033c657fb40c5a0102e90a471394e3fcd47e97e
emicludo <86956780+emicludo@users.noreply.github.com>
2022-05-04 12:55:40 +0200

    Added a Procfile.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+
+
+
+app.get('/', function(req, res){
+ /*  res.sendFile(__dirname + '/public/movedpage.html'); */
+  res.set('location', '/destination');
+  res.status(301).send()
+}); 
+app.get('/destination', function(req, res){
+  res.set('location', '/destination2');
+  res.status(301).send()
+}); 
+app.get('/destination2', function(req, res){
+  res.sendFile(__dirname + '/public/finalfinalpage.html');
+}); 
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000!")
+});
