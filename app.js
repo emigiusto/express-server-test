93f36a4cd9ad490f9b3402defd6c80179ac780f3
emicludo <86956780+emicludo@users.noreply.github.com>
2022-05-04 13:55:08 +0200

    cleanUp

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
 
-
-
 app.get('/', function(req, res){
   /* res.sendFile(__dirname + '/public/movedpage.html'); */
-  res.set('location', '/destination2');
+  res.set('location', '/destination');
   res.status(301).send()
 
 }); 
-/* app.get('/destination', function(req, res){
-  res.set('location', '/destination2');
-  res.status(301).send()
-});  */
-app.get('/destination2', function(req, res){
-  res.sendFile(__dirname + '/public/finalfinalpage.html');
+app.get('/destination', function(req, res){
+  res.sendFile(__dirname + '/public/finalpage.html');
 }); 
 
 app.listen(process.env.PORT || 5000, function(){
