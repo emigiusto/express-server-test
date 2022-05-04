1163c57ce01aff9dd44257f8acdcb8158e600a5c
emicludo <86956780+emicludo@users.noreply.github.com>
2022-05-04 13:49:22 +0200

    moved "/"

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ app.use(express.static('public')); /* this line tells Express to use the public 
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/movedpage.html');
-/*   res.set('location', '/destination');
-  res.status(301).send() */
-  
-}); 
-app.get('/destination', function(req, res){
+  /* res.sendFile(__dirname + '/public/movedpage.html'); */
   res.set('location', '/destination2');
   res.status(301).send()
+
 }); 
+/* app.get('/destination', function(req, res){
+  res.set('location', '/destination2');
+  res.status(301).send()
+});  */
 app.get('/destination2', function(req, res){
   res.sendFile(__dirname + '/public/finalfinalpage.html');
 }); 
